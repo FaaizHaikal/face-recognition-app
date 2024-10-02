5c39350e27c1c7e56f9b27b0f9e8b823d1555c2f
FaaizHaikal <faaizhaikal@gmail.com>
2024-10-02 13:09:56 +0700

    feat: track route change

--- client/src/components/TrackRouteChange.jsx
+++ client/src/components/TrackRouteChange.jsx
@@ -3,17 +3,17 @@ import { useLocation } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 
 function TrackRouteChange() {
-  const { setIsPhotoTaken, setCapturedImage } = useContext(AppContext)
+  const { setIsPhotoTaken, setCapturedImage } = useContext(AppContext);
   const location = useLocation();
 
   useEffect(() => {
-    switch(location.pathname) {
+    switch (location.pathname) {
       case '/':
         setIsPhotoTaken(false);
         setCapturedImage(null);
         break;
     }
-  }, [location])
+  }, [location]);
 
   return null;
 }
